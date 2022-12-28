174928d1e6fcaf8c03ba2d819e8d56ecf2475d9c
Aakash Dubey <110174617+skydubey@users.noreply.github.com>
2022-12-28 11:21:12 +0530

    First Commit
    Employee Portal with Validation & Data Pass Between Controller to View And View to Controller and View to View.

---
+++ Employee.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PracticeMVC14.Models
+{
+    public class Employee
+    {
+        [Required (ErrorMessage = "The Employee Id is mandatory.")]
+        public int EmpId { get; set; }
+
+
+        [Required (ErrorMessage = "The Employee Name is mandatory.")]
+        [StringLength(25,MinimumLength = 10,ErrorMessage ="Name should be in between 10 & 25.")]
+        public string EmpName { get; set; }
+        
+        [Required (ErrorMessage = "The Employee Mobile is mandatory.")]
+        [RegularExpression("^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number.")]
+        public string EmpMobile { get; set; }
+
+
+        [Required (ErrorMessage = "The Employee Age is mandatory.")]
+        [Range(18, 30,ErrorMessage ="Age must be between 18 to 30")]
+        public int? EmpAge { get; set; }
+
+
+        [Required (ErrorMessage = "The Employee Gender is mandatory.")]
+        public string EmpGender { get; set; }
+
+
+
+        [Required (ErrorMessage = "The Employee Email is mandatory.")]
+        [RegularExpression("^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$",ErrorMessage ="Email is Invalid")]
+        public string EmpEmail { get; set; }
+
+
+
+        [Required (ErrorMessage = "The Employee Password is mandatory.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}", ErrorMessage ="Password is Invalid")]
+        [DataType(DataType.Password)]
+        public string EmpPassword { get; set; }
+
+
+
+        [Required (ErrorMessage = "The Confirm Password is mandatory.")]
+        [Compare("EmpPassword",ErrorMessage ="Password & Confirm Password does not match.")]
+        public string EmpConfirmPassword { get; set; }
+
+
+
+        [ReadOnly(true)]
+        public string EmpOrganisationName { get; set; }
+
+
+
+        [Required (ErrorMessage = "The Employee Address is mandatory.")]
+        [StringLength(100, MinimumLength = 25, ErrorMessage = "Address should be in between 25 & 100")]
+        [DataType(DataType.MultilineText)]
+        public string EmpAddress { get; set; }
+
+
+
+
+        [Required (ErrorMessage = "The Employee Joining Date is mandatory.")]
+        [DataType(DataType.Date)]
+        public string EmpJoiningDate { get; set; }
+
+
+
+        /*[Required (ErrorMessage = "The Employee Joining Time is mandatory.")]
+        [DataType(DataType.Time)]
+        public string EmpJoiningTime { get; set; }*/
+
+
+
+        [Required(ErrorMessage = "The Employee Country is mandatory.")]
+        public string EmpCountry { get; set; }
+
+
+
+    }
+   
+}
---
+++ HomeController.cs
@@ -0,0 +1,63 @@
+﻿using PracticeMVC14.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeMVC14.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var CountryList = new List<string> { "India", "China", "America", "Japan" };
+            ViewBag.CountryList = CountryList;
+            return View();
+        }
+        public ActionResult Details()
+        {
+            TempData.Keep();
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Index(Employee emp)
+        {
+
+            var CountryList = new List<string> { "India", "China", "America", "Japan" };
+            ViewBag.CountryList = CountryList;
+
+            emp.EmpOrganisationName = "Aakash Investments";
+
+
+            if (ModelState.IsValid == true) 
+            {
+                ViewData["SuccessMessage"] = "<script>Swal.fire(\r\n  'Data has been submitted succesfully!',\r\n  'Thank You!',\r\n  'success'\r\n)</script>";
+                ModelState.Clear();
+                RedirectToAction("Details");
+            }
+
+
+
+            TempData["empId"] = emp.EmpId;
+            TempData["empName"] = emp.EmpName;
+            TempData["empEmail"] = emp.EmpEmail;
+            TempData["empMobile"] = emp.EmpMobile;
+            TempData["empOrgainsationName"] = emp.EmpOrganisationName;
+            TempData["empGender"] = emp.EmpGender;
+            TempData["empAge"] = emp.EmpAge;
+            TempData["empPassword"] = emp.EmpPassword;
+            TempData["empJoiningDate"] = emp.EmpJoiningDate;
+            TempData["empCountry"] = emp.EmpCountry;
+            TempData["empAddress"] = emp.EmpAddress;
+
+            TempData.Keep();
+
+            return View();
+        }
+    }
+}
